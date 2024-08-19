3438eebccb653bdc4c3fa2b6cdbf609e68ddd948
S. Rahman <alphaseawarrior@proton.me>
2024-08-20 00:12:03 +0600

    Front End development
    React based framework initialized. Test landing page is functional. Routes and component works are in progress.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
