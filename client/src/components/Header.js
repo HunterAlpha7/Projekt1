032516534b78661f378c1eddca094ec25c8b8786
S. Rahman <alphaseawarrior@proton.me>
2024-08-29 14:11:01 +0600

    Login, logout, register functionality complete.
    Registration is now integrated with the back-end database. Issue #2, #3 has been solved.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,41 +1,83 @@
-import React,{useState} from 'react';
-import {Box, AppBar,Toolbar, Button,Typography, Tabs, Tab} from '@mui/material';
-import { Link } from 'react-router-dom';
-import {useSelector} from 'react-redux';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../redux/store";
 const Header = () => {
-    // global state
-    const isLogin = useSelector(state => state.isLogin)
-    console.log(isLogin);
-    //state
-    const [value,setValue] = useState()
+  // global state
+  const isLogin = useSelector((state) => state.isLogin);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  //state
+  const [value, setValue] = useState();
+
+  //logout
+  const handleLogout = () => {
+    try {
+      dispatch(authActions.logout());
+      alert("Sudo apt get-out. Bye");
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-    <AppBar position='sticky'>
+      <AppBar position="sticky">
         <Toolbar>
-            <Typography variant='h4'>
-                My Blog App
-            </Typography>
-            {isLogin && (<Box display={'flex'} marginLeft="auto" marginRight="auto">
-                <Tabs textColor="inherit" value={value} onChange={(e,val)=> setValue(val)}>
-                    <Tab label = "Blogs" LinkComponent={Link} to="/blogs" />
-                    <Tab label = "My Blogs" LinkComponent={Link} to="/my-blogs" />
-                </Tabs>
-            </Box>)}
-            <Box display={'flex'} marginLeft="auto">
-
-            {!isLogin && (<>
-            <Button sx={{margin:1,color:'white'}} LinkComponent={Link} to="./login">Login</Button>
-            <Button sx={{margin:1,color:'white'}} LinkComponent={Link} to="./register">Register</Button></>)
-            }
-
-            {isLogin && (<Button sx={{margin:1,color:'white'}}>Logout</Button>)}
-                
+          <Typography variant="h4">My Blog App</Typography>
+          {isLogin && (
+            <Box display={"flex"} marginLeft="auto" marginRight="auto">
+              <Tabs
+                textColor="inherit"
+                value={value}
+                onChange={(e, val) => setValue(val)}
+              >
+                <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
+                <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs" />
+              </Tabs>
             </Box>
-        </Toolbar>
+          )}
+          <Box display={"flex"} marginLeft="auto">
+            {!isLogin && (
+              <>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="./login"
+                >
+                  Login
+                </Button>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="./register"
+                >
+                  Register
+                </Button>
+              </>
+            )}
 
-    </AppBar>
+            {isLogin && (
+              <Button onClick={handleLogout} sx={{ margin: 1, color: "white" }}>
+                Logout
+              </Button>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
     </>
-  )
-}
+  );
+};
 
 export default Header;
